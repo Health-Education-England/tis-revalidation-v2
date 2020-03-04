6b7d60b51f56489bf8b741aa020f9b97b6b5d9f3
Omar Mirza <omar.creative.dev@gmail.com>
2020-03-04 15:01:17 +0000

    feat: routing strategy for revalidation summary view
    TISNEW-4021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/dashboard",
+    pathMatch: "full"
+  },
+  {
+    path: "dashboard",
+    loadChildren: () =>
+      import("./features/dashboard/dashboard.module").then(
+        m => m.DashboardModule
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/features/dashboard/dashboard.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DashboardPageComponent } from "../pages/dashboard-page/dashboard-page.component";
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+
+@NgModule({
+  declarations: [DashboardPageComponent],
+  imports: [CommonModule, DashboardRoutingModule]
+})
+export class DashboardModule {}

ed059669f86896c12279b47bc8f3894176bd7b1d
Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>
2020-03-06 11:45:01 +0000

    adjusted lint settings for vscode
    added IE polyfills
    
    [TISNEW-4026-browser-matrix]

--- src/assets/browser-support/browser.support.js
+++ src/assets/browser-support/browser.support.js
@@ -1,10 +1,11 @@
-
-(function () {
-    try {
-        var isIE = (/*@cc_on ! @*/ false);
-        var docMode = document.documentMode;
-        if (isIE && docMode === 10) {
-            window.location.replace("assets/browser-support/browser-not-supported.html");
-        }
-    } catch{}
+(function() {
+  try {
+    var isIE = /*@cc_on ! @*/ false;
+    var docMode = document.documentMode;
+    if (isIE && docMode === 10) {
+      window.location.replace(
+        "assets/browser-support/browser-not-supported.html"
+      );
+    }
+  } catch {}
 })();

b1fc96a707dd54bbe7d8b0937c4ffd2537aa37dd
Omar Mirza <omar.creative.dev@gmail.com>
2020-03-09 15:40:56 +0000

    feat: page not found route
    TISNEW-4044

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { PageNotFoundComponent } from "./shared/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     path: "dashboard",
     loadChildren: () =>
       import("./dashboard/dashboard.module").then(m => m.DashboardModule)
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
 

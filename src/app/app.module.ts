398bfa8a00742c46c83a0f623f26ba99957bd31d
Omar Mirza <omar.creative.dev@gmail.com>
2020-03-11 13:45:00 +0000

    fix: move ngxs root module back to app module in order for ngxs logger to work properly as per docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "@environment";
+import { NgxsModule } from "@ngxs/store";
 import { AnalyticsModule, HotJarModule } from "hee-shared";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -26,6 +27,9 @@ import { SharedModule } from "./shared/shared.module";
       hotJarId: 1662399,
       hotJarSv: 6,
       enabled: environment.production
+    }),
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production
     })
   ],
   providers: [],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,7 +6,6 @@ import { RouterModule } from "@angular/router";
 import { environment } from "@environment";
 import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
 import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
-import { NgxsModule } from "@ngxs/store";
 import { MaterialModule } from "../material/material.module";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
@@ -18,9 +17,6 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
     ReactiveFormsModule,
     RouterModule,
     HttpClientModule,
-    NgxsModule.forRoot([], {
-      developmentMode: !environment.production
-    }),
     NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production }),
     NgxsLoggerPluginModule.forRoot({ disabled: environment.production })
   ],

6a4e1c023304f8125fb5392bb1461aad9978a427
Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>
2020-03-13 12:05:19 +0000

    sonar cloud suggestions
    [TISNEW-4025]

--- src/app/mat-main-nav/menu-items.const.ts
+++ src/app/mat-main-nav/menu-items.const.ts
@@ -1,20 +1,21 @@
-import { IMenuItem } from "./menu-item.interface";
+import { IMenuItem, MenuType } from "./menu-item.interface";
 
 export const menuItems: IMenuItem[] = [
+  //NOSONAR
   {
     name: "People",
-    type: 1,
+    type: MenuType.external,
     route: "admin/people/person-search",
     description: "",
     menuItems: [
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/people/person-search",
         name: "Add person record",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/uploads",
         name: "Bulk upload",
         description: ""
@@ -23,24 +24,24 @@ export const menuItems: IMenuItem[] = [
   },
   {
     name: "Posts",
-    type: 1,
+    type: MenuType.external,
     route: "admin/posts",
     description: "",
     menuItems: [
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/posts/post",
         name: "Search posts",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/posts/new-post",
         name: "Create posts",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/uploads",
         name: "Bulk upload",
         description: ""
@@ -49,30 +50,30 @@ export const menuItems: IMenuItem[] = [
   },
   {
     name: "Programmes",
-    type: 1,
+    type: MenuType.external,
     route: "admin/programmes",
     description: "",
     menuItems: [
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/programmes/new-programme",
         name: "Create programmes",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/programmes/curriculum",
         name: "Curricula",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/rotations/rotations",
         name: "Rotations",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "admin/programmes/list",
         name: "Search programmes",
         description: ""
@@ -81,30 +82,30 @@ export const menuItems: IMenuItem[] = [
   },
   {
     name: "Revalidation",
-    type: 0,
+    type: MenuType.internal,
     route: "admin/manage-connections",
     description: "",
     menuItems: [
       {
-        type: 0,
+        type: MenuType.internal,
         route: "admin/manage-connections",
         name: "Manage connections",
         description: ""
       },
       {
-        type: 0,
+        type: MenuType.internal,
         route: "concerns/",
         name: "Concerns log",
         description: ""
       },
       {
-        type: 0,
+        type: MenuType.internal,
         route: "admin/manage-connections/create",
         name: "Connection discrepancies",
         description: ""
       },
       {
-        type: 0,
+        type: MenuType.internal,
         route: "revalidation/",
         name: "Provide recommendation",
         description: ""
@@ -113,216 +114,216 @@ export const menuItems: IMenuItem[] = [
   },
   {
     name: "Assessments",
-    type: 1,
+    type: MenuType.external,
     route: "admin/assessment",
     description: ""
   },
   {
     name: "Admin",
-    type: 1,
+    type: MenuType.external,
     route: "admin/reference",
     description: "",
     menuItems: [
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/college",
         name: "College",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/country",
         name: "Country",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/programmes/curriculum",
         name: "Curricula",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/curriculum-sub-type",
         name: "Curriculum SubType",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/dbc",
         name: "Dbc",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/ethnic-origin",
         name: "Ethnic Origin",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/funding-issue",
         name: "Funding Issue",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/funding-type",
         name: "Funding Type",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/gdc-status",
         name: "Gdc Status",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/gender",
         name: "Gender",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/gmc-status",
         name: "Gmc Status",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/grade",
         name: "Grade",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/inactive-reason",
         name: "Inactive Reason",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/leaving-destination",
         name: "Leaving Destination",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/leaving-reason",
         name: "Leaving Reason",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/local-office",
         name: "Local Office",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/marital-status",
         name: "Marital Status",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/medical-school",
         name: "Medical School",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/nationality",
         name: "Nationality",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/placement-type",
         name: "Placement Type",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/programme-membership-type",
         name: "Programme Membership Type",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/record-type",
         name: "Record Type",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/religious-belief",
         name: "Religious Belief",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/role",
         name: "Role",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/settled",
         name: "Settled",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/sexual-orientation",
         name: "Sexual Orientation",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/site",
         name: "Site",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/programmes/specialty",
         name: "Specialty",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/programmes/specialty-group",
         name: "Specialty Group",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/status",
         name: "Status",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/tariff-rate",
         name: "Tariff Rate",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/title",
         name: "Title",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/training-number-type",
         name: "Training Number Type",
         description: ""
       },
       {
-        type: 1,
+        type: MenuType.external,
         route: "/admin/reference/trust",
         name: "Trust",
         description: ""

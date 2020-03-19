4f9dd8303011d2e12a48ab14428886c893219ffe
Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>
2020-03-19 14:44:21 +0000

    fix: browser support refactor
    added borwserlist-useragent-regexp node module
    created node script to build js for app browser detection
    revised html template for browser not supported

---
+++ supported.browsers.node.ts
@@ -0,0 +1,29 @@
+const browserRegEx = require("browserslist-useragent-regexp");
+
+if (!browserRegEx) {
+  throw new Error(
+    "browserslist-useragent-regexp is needed, please run npm browserslist-useragent-regexp --save-dev"
+  );
+}
+
+const supportedRegEx = browserRegEx.getUserAgentRegExp({
+  allowHigherVersions: true
+});
+const fs = require("fs");
+
+const content = `(function() {
+  var sb = ${supportedRegEx};
+  if (sb.test(navigator.userAgent) === false) {
+    window.location.replace(
+      "assets/browser-support/browser-not-supported.html"
+    );
+  }
+})();
+`;
+
+fs.writeFile("supported.browsers.js", content, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});

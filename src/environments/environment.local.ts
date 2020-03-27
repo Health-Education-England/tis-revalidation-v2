0a81aa4b7dcff3ba63f784742a8a08a45a574f26
Omar Mirza <omar.creative.dev@gmail.com>
2020-03-27 05:54:00 +0000

    refactor: fe updates due to api changes
    TISNEW-4088

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -4,14 +4,14 @@
 import { IEnvironment } from "./environment.interface";
 export const environment: IEnvironment = {
   production: false,
-  host: `mocky/`,
+  host: `api/`,
   siteIds: [],
   adminsUIHostUri: `https://dev-apps.tis.nhs.uk/`,
   supportLink: `https://teams.microsoft.com/l/channel/19%3ac7943c6ffa9c49b881304863bb39ff7b%40thread.skype/General?groupId=102f33a3-f794-4089-8c5a-68e04897e72e&tenantId=ffa7912b-b097-4131-9c0f-d0e80755b2ab`,
   appUrls: {
     login: ``,
     authRedirect: ``,
-    getUnderNoticeTrainees: `5e7cd87a3500007e5b069c79?mocky-delay=700ms`
+    getUnderNoticeTrainees: `v1/doctors`
   }
 };
 

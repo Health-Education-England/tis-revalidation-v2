7dc81d0aa4aba88266aa2c46cc70711883742876
Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>
2020-04-12 10:40:30 +0100

    refactor: fixes for sonar cloud flagged duplicates

--- src/app/trainees/resolvers/trainees-under-notice.resolver.spec.ts
+++ src/app/trainees/resolvers/trainees-under-notice.resolver.spec.ts
@@ -5,17 +5,17 @@ import { TraineesState } from "../state/trainees.state";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("TraineesUnderNotice.Resolver", () => {
-  let store: Store;
+  let underNoticeStore: Store;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, NgxsModule.forRoot([TraineesState])],
       providers: []
     }).compileComponents();
-    store = TestBed.inject(Store);
+    underNoticeStore = TestBed.inject(Store);
   }));
 
   it("should create an instance", () => {
-    expect(new TraineesUnderNoticeResolver(store)).toBeTruthy();
+    expect(new TraineesUnderNoticeResolver(underNoticeStore)).toBeTruthy();
   });
 });
--- src/app/trainees/resolvers/trainees.resolver.spec.ts
+++ src/app/trainees/resolvers/trainees.resolver.spec.ts
@@ -2,20 +2,25 @@ import { TraineesResolver } from "./trainees.resolver";
 import { Store, NgxsModule } from "@ngxs/store";
 import { TestBed, async } from "@angular/core/testing";
 import { TraineesState } from "../state/trainees.state";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 
 describe("TraineesResolver", () => {
-  let store: Store;
+  let traineeStore: Store;
+  let httpMock: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, NgxsModule.forRoot([TraineesState])],
       providers: []
     }).compileComponents();
-    store = TestBed.inject(Store);
+    traineeStore = TestBed.inject(Store);
+    httpMock = TestBed.inject(HttpTestingController);
   }));
 
   it("should create an instance", () => {
-    expect(new TraineesResolver(store)).toBeTruthy();
+    expect(new TraineesResolver(traineeStore)).toBeTruthy();
   });
 });

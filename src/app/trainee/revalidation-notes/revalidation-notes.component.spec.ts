29304f49fc92a5d628ad93bdeba270dcc1348b5c
Omar Mirza <omar.creative.dev@gmail.com>
2020-04-28 16:01:24 +0100

    test: fix broken unit tests

--- src/app/trainee/revalidation-notes/revalidation-notes.component.spec.ts
+++ src/app/trainee/revalidation-notes/revalidation-notes.component.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxsModule } from "@ngxs/store";
+import { RevalidationHistoryState } from "../state/revalidation-history.state";
 
 import { RevalidationNotesComponent } from "./revalidation-notes.component";
 import { MaterialModule } from "src/app/shared/material/material.module";
@@ -12,7 +15,12 @@ describe("RevalidationNotesComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule, MaterialModule],
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+        HttpClientTestingModule,
+        NgxsModule.forRoot([RevalidationHistoryState])
+      ],
       declarations: [RevalidationNotesComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [{ provide: MatBottomSheetRef, useValue: {} }]

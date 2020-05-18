7ac4241e2e1b198b475a9ff2cf47beef4712705b
Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>
2020-05-18 13:32:11 +0100

    feat: UI optimization
    removed sticky navigation
    revised desktop navigation from 2 rows to one row
    fixed bug with skip to maincontent link
    
    TISNEW-4577

--- src/app/shared/main-navigation/mat-main-nav/mat-main-nav.component.ts
+++ src/app/shared/main-navigation/mat-main-nav/mat-main-nav.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 import { MatIconRegistry } from "@angular/material/icon";
 import { Observable } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
+import { map, shareReplay, filter } from "rxjs/operators";
 import { DomSanitizer } from "@angular/platform-browser";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-mat-main-nav",
   templateUrl: "./mat-main-nav.component.html",
   styleUrls: ["./mat-main-nav.component.scss"]
 })
-export class MatMainNavComponent {
+export class MatMainNavComponent implements OnInit {
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -18,13 +19,26 @@ export class MatMainNavComponent {
       shareReplay()
     );
   @Input() skipLinkSelector = "maincontent";
+  activeUrl: string;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private router: Router
   ) {
     this.addNHSIcon();
+  }
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const hashLocation = `#${this.skipLinkSelector}`;
+        this.activeUrl = `${this.router.url}`;
+        this.activeUrl = this.activeUrl.endsWith(hashLocation)
+          ? this.activeUrl
+          : this.activeUrl + hashLocation;
+      });
   }
 
   addNHSIcon() {

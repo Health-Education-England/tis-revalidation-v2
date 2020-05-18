d5ecbfedc42cb4d9e3fe19b4540a6e5b1b2ee0a9
ojemuyiwa <ojemuyiwa@users.noreply.github.com>
2020-05-18 15:01:33 +0100

    feat: UI optimisation (#80)
    * feat: UI optimization
    
    removed sticky navigation
    revised desktop navigation from 2 rows to one row
    fixed bug with skip to maincontent link
    
    TISNEW-4577
    
    * fix: PR suggestions
    
    unsubscribe from observable
    
    TISNEW-4577
    
    Co-authored-by: Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>

--- src/app/shared/main-navigation/mat-main-nav/mat-main-nav.component.ts
+++ src/app/shared/main-navigation/mat-main-nav/mat-main-nav.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit, OnDestroy } from "@angular/core";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 import { MatIconRegistry } from "@angular/material/icon";
-import { Observable } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
+import { Observable, Subscription } from "rxjs";
+import { map, shareReplay, filter } from "rxjs/operators";
 import { DomSanitizer } from "@angular/platform-browser";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-mat-main-nav",
   templateUrl: "./mat-main-nav.component.html",
   styleUrls: ["./mat-main-nav.component.scss"]
 })
-export class MatMainNavComponent {
+export class MatMainNavComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -18,13 +20,31 @@ export class MatMainNavComponent {
       shareReplay()
     );
   @Input() skipLinkSelector = "maincontent";
+  activeUrl: string;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private router: Router
   ) {
     this.addNHSIcon();
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const hashLocation = `#${this.skipLinkSelector}`;
+        this.activeUrl = `${this.router.url}`;
+        this.activeUrl = this.activeUrl.endsWith(hashLocation)
+          ? this.activeUrl
+          : this.activeUrl + hashLocation;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   addNHSIcon() {

fe3382d146aeedbaa576c01202cac39965c64273
Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>
2020-06-08 19:13:56 +0100

    fix: use angular injector for document
    TISNEW-4702

--- src/app/shared/strip-html.pipe.ts
+++ src/app/shared/strip-html.pipe.ts
@@ -1,18 +1,20 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform, Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
 
 @Pipe({
   name: "stripHtml",
   pure: true
 })
 export class StripHtmlPipe implements PipeTransform {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
   transform(htmlString: string, ...args: any[]): string {
     return (
-      document
+      this.document
         .createRange()
         // Creates a fragment and turns the supplied string into HTML nodes
         .createContextualFragment(htmlString)
         // Gets the text from the fragment
-        .textContent// Removes the Zero-Width Space, Zero-Width Joiner, Zero-Width No-Break Space, Left-To-Right Mark, and Right-To-Left Mark characters
+        .textContent // Removes the Zero-Width Space, Zero-Width Joiner, Zero-Width No-Break Space, Left-To-Right Mark, and Right-To-Left Mark characters
         .replace(/[\u200B-\u200D\uFEFF\u200E\u200F]/g, "")
         // Trims off any extra space on either end of the string
         .trim()

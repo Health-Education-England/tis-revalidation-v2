7d638b254637fdb793c06ddfd0da5c957acc8d4c
Femi Ojemuyiwa <femi.ojemuyiwa@vanquisbank.co.uk>
2020-06-09 02:12:26 +0100

    fix: broken unit test
    added UI enhancement for wide typing of comments

--- src/app/shared/strip-html.pipe.spec.ts
+++ src/app/shared/strip-html.pipe.spec.ts
@@ -1,8 +1,32 @@
-import { StripHtmlPipe } from './strip-html.pipe';
+import { StripHtmlPipe } from "./strip-html.pipe";
+import { DOCUMENT } from "@angular/common";
+import { TestBed } from "@angular/core/testing";
 
-describe('StripHtmlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StripHtmlPipe();
+describe("StripHtmlPipe", () => {
+  let _document: Document;
+
+  beforeEach(() => {
+    _document = TestBed.inject(DOCUMENT);
+  });
+
+  it("create an instance", () => {
+    const pipe = new StripHtmlPipe(_document);
     expect(pipe).toBeTruthy();
+  });
+
+  it("one level html", () => {
+    const pipe = new StripHtmlPipe(_document);
+    const txt = pipe.transform("<div>the rain in spain</div>");
+    expect(txt).toEqual("the rain in spain");
+  });
+
+  it("multi level html", () => {
+    const pipe = new StripHtmlPipe(_document);
+    const txt = pipe.transform(`
+    <div>
+    <h1>the rain in spain</h1> <p>stays mainly in <em>the plane</em></p>
+    </div>
+    `);
+    expect(txt).toEqual(`the rain in spain stays mainly in the plane`);
   });
 });

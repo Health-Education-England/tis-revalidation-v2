2bbcdcabb6994428280eae99ec597b82551e0add
Omar Mirza <omar.creative.dev@gmail.com>
2020-06-25 11:15:17 +0100

    refactor: extract common environment config (#205)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+import { APP_URLS_CONFIG } from "./constants";
 import { IEnvironment } from "./environment.interface";
 export const environment: IEnvironment = {
   production: true,
@@ -7,18 +8,7 @@ export const environment: IEnvironment = {
   adminsUIHostUri: `https://apps.tis.nhs.uk/`,
   supportLink: `https://teams.microsoft.com/l/channel/19%3ac7943c6ffa9c49b881304863bb39ff7b%40thread.skype/General?groupId=102f33a3-f794-4089-8c5a-68e04897e72e&tenantId=ffa7912b-b097-4131-9c0f-d0e80755b2ab`,
   dateFormat: "dd/MM/yyyy",
-  appUrls: {
-    authRedirect: ``,
-    getConcerns: `api/concerns`,
-    getConnections: `api/v1/connections`,
-    getNotes: `mocky/5ea2da614f00006c00d9f540`, // TODO replace with appropriate api url once is available
-    getRecommendation: `api/recommendation`,
-    getRecommendations: `api/v1/doctors`,
-    login: ``,
-    saveRecommendation: `api/recommendation`,
-    getDetails: `api/trainee`,
-    submitToGMC: `api/recommendation/{gmcNumber}/submit/{recommendationId}`
-  },
+  appUrls: APP_URLS_CONFIG,
   // TODO add correct prod values below
   awsConfig: {
     userPoolWebClientId: "3adscm2usl3lop510nfijpr12f",

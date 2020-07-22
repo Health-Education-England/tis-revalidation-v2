d5ce0016576923e9c4be57a66838ed25ad389a97
Omar Mirza <omar.creative.dev@gmail.com>
2020-07-22 16:07:01 +0100

    fix: http error handling | pr feedback
    NO-TICKEt

--- src/app/shared/services/error/error.service.ts
+++ src/app/shared/services/error/error.service.ts
@@ -6,17 +6,9 @@ import { Injectable } from "@angular/core";
 })
 export class ErrorService {
   public generateErrorMsg(error: HttpErrorResponse): string {
-    let message: string;
-
-    if (error.error instanceof ErrorEvent) {
-      // client-side error
-      message = `Error: ${error.error.message}`;
-    } else {
-      // server-side error
-      message = `Error Code: ${error.status}. ${error.statusText}`;
-    }
-
-    return message;
+    return error.error instanceof ErrorEvent
+      ? `Error: ${error.error.message}`
+      : `Error Code: ${error.status}. ${error.statusText}`;
   }
 }
 

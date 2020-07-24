c560188a999f07fc7482ba13457e8231b241ae69
Omar Mirza <omar.creative.dev@gmail.com>
2020-07-24 10:47:42 +0100

    fix: remove get recommendations call
    NO-TICKET

--- src/app/recommendation/recommendation.resolver.ts
+++ src/app/recommendation/recommendation.resolver.ts
@@ -5,12 +5,11 @@ import {
   Router
 } from "@angular/router";
 import { IRecommendationHistory } from "./recommendation-history.interface";
-import { Observable, forkJoin } from "rxjs";
+import { Observable } from "rxjs";
 import { Store } from "@ngxs/store";
 import { catchError } from "rxjs/operators";
 import { Injectable } from "@angular/core";
 import { Get as GetRecommendationHistory } from "./state/recommendation-history.actions";
-import { GetRecommendationNotes } from "./state/recommendation-notes.actions";
 
 @Injectable()
 export class RecommendationResolver implements Resolve<IRecommendationHistory> {
@@ -21,11 +20,8 @@ export class RecommendationResolver implements Resolve<IRecommendationHistory> {
     state: RouterStateSnapshot
   ): Observable<IRecommendationHistory> | Observable<any> {
     const gmcNumber: number = Number(route.params.gmcNumber);
-    return forkJoin([
-      this.store
-        .dispatch(new GetRecommendationHistory(gmcNumber))
-        .pipe(catchError(() => this.router.navigate(["/404"]))),
-      this.store.dispatch(new GetRecommendationNotes(gmcNumber))
-    ]);
+    return this.store
+      .dispatch(new GetRecommendationHistory(gmcNumber))
+      .pipe(catchError(() => this.router.navigate(["/404"])));
   }
 }

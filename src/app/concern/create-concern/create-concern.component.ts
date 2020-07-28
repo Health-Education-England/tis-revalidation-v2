c79346f7b2cd64636b5938c3ce035015a768bb79
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2020-07-28 15:47:14 +0100

    feat: create concern
    added wizard control and broken down and created concern-detail form
    
    TISNEW-5043

--- src/app/concern/create-concern/create-concern.component.ts
+++ src/app/concern/create-concern/create-concern.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
+import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
 
 @Component({
   selector: "app-create-concern",
-  templateUrl: "./create-concern.component.html"
+  templateUrl: "./create-concern.component.html",
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true }
+    }
+  ]
 })
 export class CreateConcernComponent {
   // TODO cater for edit mode

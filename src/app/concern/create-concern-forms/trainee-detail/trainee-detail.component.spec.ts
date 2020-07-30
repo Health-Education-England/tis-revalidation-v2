48058953ae4b8d5cf916702b82df227795b7426b
Omar Mirza <omar.creative.dev@gmail.com>
2020-07-30 15:54:46 +0100

    feat: fix specs
    TISNEW-5043

--- src/app/concern/create-concern-forms/trainee-detail/trainee-detail.component.spec.ts
+++ src/app/concern/create-concern-forms/trainee-detail/trainee-detail.component.spec.ts
@@ -1,14 +1,26 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxsModule } from "@ngxs/store";
+import { AdminsState } from "../../../admins/state/admins.state";
+import { MaterialModule } from "../../../shared/material/material.module";
+import { ConcernState } from "../../state/concern.state";
 
 import { TraineeDetailComponent } from "./trainee-detail.component";
 
-describe("TraineeDetailComponent", () => {
+fdescribe("TraineeDetailComponent", () => {
   let component: TraineeDetailComponent;
   let fixture: ComponentFixture<TraineeDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TraineeDetailComponent]
+      declarations: [TraineeDetailComponent],
+      imports: [
+        MaterialModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        NgxsModule.forRoot([ConcernState, AdminsState])
+      ]
     }).compileComponents();
   }));
 

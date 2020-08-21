a80198a709ab8f75cc1edffbb82532bea76bdfe4
Femi Ojemuyiwa <ojemuyiwa@gmail.com>
2020-08-21 15:43:34 +0100

    fix: improve test coverage for hee-shared

--- projects/hee-shared/src/lib/analytics/analytics.module.spec.ts
+++ projects/hee-shared/src/lib/analytics/analytics.module.spec.ts
@@ -7,41 +7,51 @@ import { AnalyticsConfig, AnalyticsModule } from "./analytics.module";
 describe("MockComponent", () => {
   let router: Router;
   let fixture: any;
-  const analyticsConfig: AnalyticsConfig = { siteId: ["TEST"], enabled: true };
+  let docHead: HTMLHeadElement;
+  let scriptTag: HTMLScriptElement;
+  let asyncTag: boolean;
+  const scriptQuerySelector =
+    'script[src^="https://www.google-analytics.com/analytics.js"]';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule.withRoutes(routes),
-        AnalyticsModule.forRoot(analyticsConfig)
-      ],
-      declarations: [AppComponent, MockComponent]
-    }).compileComponents();
-
-    router = TestBed.inject(Router);
-    fixture = TestBed.createComponent(AppComponent);
-
-    fixture.ngZone.run(() => {
-      router.initialNavigation();
-    });
-  }));
-
-  it("should create the mock component", () => {
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  const setVariables = () => {
+    docHead = fixture.debugElement.nativeNode.ownerDocument.head;
+    scriptTag = docHead.querySelector(scriptQuerySelector);
+    asyncTag = scriptTag ? scriptTag.hasAttribute("async") : false;
+  };
 
   describe("Analytics Module loader tests", () => {
-    let docHead: HTMLHeadElement;
-    let scriptTag: HTMLScriptElement;
-    let asyncTag: boolean;
+    const analyticsConfig: AnalyticsConfig = {
+      siteId: ["TEST"],
+      enabled: true
+    };
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule.withRoutes(routes),
+          AnalyticsModule.forRoot(analyticsConfig)
+        ],
+        declarations: [AppComponent, MockComponent]
+      }).compileComponents();
+
+      router = TestBed.inject(Router);
+      fixture = TestBed.createComponent(AppComponent);
+
+      fixture.ngZone.run(() => {
+        router.initialNavigation();
+      });
+
+      fixture.detectChanges();
+      docHead = scriptTag = asyncTag = null;
+    });
 
     beforeEach(() => {
-      docHead = fixture.debugElement.nativeNode.ownerDocument.head;
-      scriptTag = docHead.querySelector(
-        'script[src^="https://www.google-analytics.com/analytics.js"]'
-      );
-      asyncTag = scriptTag.hasAttribute("async");
+      setVariables();
+    });
+
+    it("should create the mock component", () => {
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
     });
 
     it("header tag should contain the script tag", () => {
@@ -50,6 +60,49 @@ describe("MockComponent", () => {
 
     it("script tag should contain the async attribute", () => {
       expect(asyncTag).toBeTrue();
+    });
+  });
+
+  describe("Alter module configuration", () => {
+    const newAnalyticsConfig: AnalyticsConfig = {
+      siteId: ["TEST"],
+      enabled: false
+    };
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule.withRoutes(routes),
+          AnalyticsModule.forRoot(newAnalyticsConfig)
+        ],
+        declarations: [AppComponent, MockComponent]
+      }).compileComponents();
+
+      router = TestBed.inject(Router);
+      fixture = TestBed.createComponent(AppComponent);
+
+      fixture.ngZone.run(() => {
+        router.initialNavigation();
+      });
+
+      fixture.detectChanges();
+      docHead = scriptTag = asyncTag = null;
+    });
+
+    beforeEach(() => {
+      setVariables();
+    });
+
+    it("should create the mock component", () => {
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it("should not initialise or add script to header", () => {
+      expect(scriptTag).toBeFalsy();
+    });
+
+    it("should not initialise or add script to header", () => {
+      expect(asyncTag).toBeFalsy();
     });
   });
 });
--- projects/hee-shared/src/lib/hotjar/hotjar.module.spec.ts
+++ projects/hee-shared/src/lib/hotjar/hotjar.module.spec.ts
@@ -7,47 +7,52 @@ import { AppComponent, MockComponent, routes } from "../mock.test.component";
 describe("MockComponent", () => {
   let router: Router;
   let fixture: any;
-  const hotJarConfig: HotJarConfig = {
-    hotJarId: 1,
-    hotJarSv: 2,
-    enabled: true
+  let docHead: HTMLHeadElement;
+  let scriptTag: HTMLScriptElement;
+  let scriptSrc: string;
+  let asyncTag: boolean;
+
+  const setVariables = () => {
+    docHead = fixture.debugElement.nativeNode.ownerDocument.head;
+    scriptTag = docHead.querySelector(
+      'script[src^="https://static.hotjar.com"]'
+    );
+    scriptSrc = scriptTag ? scriptTag.getAttribute("src") : null;
+    asyncTag = scriptTag ? scriptTag.hasAttribute("async") : false;
   };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule.withRoutes(routes),
-        HotJarModule.forRoot(hotJarConfig)
-      ],
-      declarations: [AppComponent, MockComponent]
-    }).compileComponents();
+  describe("HotJar Enabled Module loader tests", () => {
+    let hotJarConfig: HotJarConfig = {
+      hotJarId: 1,
+      hotJarSv: 2,
+      enabled: true
+    };
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule.withRoutes(routes),
+          HotJarModule.forRoot(hotJarConfig)
+        ],
+        declarations: [AppComponent, MockComponent]
+      }).compileComponents();
 
-    router = TestBed.inject(Router);
-    fixture = TestBed.createComponent(AppComponent);
+      router = TestBed.inject(Router);
+      fixture = TestBed.createComponent(AppComponent);
 
-    fixture.ngZone.run(() => {
-      router.initialNavigation();
+      fixture.ngZone.run(() => {
+        router.initialNavigation();
+      });
+
+      fixture.detectChanges();
     });
-  }));
-
-  it("should create the mock component", () => {
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  describe("HotJar Module loader tests", () => {
-    let docHead: HTMLHeadElement;
-    let scriptTag: HTMLScriptElement;
-    let scriptSrc: string;
-    let asyncTag: boolean;
 
     beforeEach(() => {
-      docHead = fixture.debugElement.nativeNode.ownerDocument.head;
-      scriptTag = docHead.querySelector(
-        'script[src^="https://static.hotjar.com"]'
-      );
-      scriptSrc = scriptTag.getAttribute("src");
-      asyncTag = scriptTag.hasAttribute("async");
+      setVariables();
+    });
+
+    it("should create the mock component", () => {
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
     });
 
     it("header tag should contain the script tag", () => {
@@ -64,6 +69,61 @@ describe("MockComponent", () => {
 
     it("script tag should contain the async attribute ", () => {
       expect(asyncTag).toBeTrue();
+    });
+
+    afterEach(() => {
+      docHead = scriptTag = asyncTag = scriptSrc = null;
+    });
+  });
+
+  describe("HotJar Disabled Module loader tests", () => {
+    const disabledHotJarConfig: HotJarConfig = {
+      hotJarId: 1,
+      hotJarSv: 2,
+      enabled: false
+    };
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule.withRoutes(routes),
+          HotJarModule.forRoot(disabledHotJarConfig)
+        ],
+        declarations: [AppComponent, MockComponent]
+      }).compileComponents();
+
+      router = TestBed.inject(Router);
+      fixture = TestBed.createComponent(AppComponent);
+
+      fixture.ngZone.run(() => {
+        router.initialNavigation();
+      });
+
+      fixture.detectChanges();
+    });
+
+    beforeEach(() => {
+      setVariables();
+    });
+
+    it("should create the mocked component", () => {
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it("header tag should not contain the script tag", () => {
+      expect(scriptTag).toBeNull();
+    });
+
+    it("script tag should not exist", () => {
+      expect(scriptSrc).toBeNull();
+    });
+
+    it("script tag should not contain the async attribute ", () => {
+      expect(asyncTag).toBeFalse();
+    });
+
+    afterEach(() => {
+      docHead = scriptTag = asyncTag = scriptSrc = null;
     });
   });
 });

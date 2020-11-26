be1b1b56af6cc96157728a858963db946f4fe703
Babul Reddy Yasa <babul3012@gmail.com>
2020-11-26 12:53:55 +0000

    refactor: Created a re-usable confirm dialog

--- src/app/shared/confirm-dialog/confirm-dialog.component.spec.ts
+++ src/app/shared/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { ConfirmDialogComponent } from "./confirm-dialog.component";
 
 describe("ConfirmDialogComponent", () => {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+
+import { MaterialModule } from "./material/material.module";
+import { ConfirmDialogComponent } from "./confirm-dialog/confirm-dialog.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { ApiService } from "./services/api/api.service";
 import { StripHtmlPipe } from "./strip-html.pipe";
@@ -7,8 +10,8 @@ import { FileBytesPipe } from "./file-bytes.pipe";
 
 const modulePipes = [StripHtmlPipe, FileBytesPipe];
 @NgModule({
-  declarations: [PageNotFoundComponent, ...modulePipes],
-  imports: [RouterModule],
+  declarations: [PageNotFoundComponent, ConfirmDialogComponent, ...modulePipes],
+  imports: [RouterModule, MaterialModule],
   exports: [...modulePipes],
   providers: [ApiService]
 })

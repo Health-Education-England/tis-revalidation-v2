490e66eceb79df544e29cf6d897e0b69a1befdf5
Babul Reddy Yasa <babul3012@gmail.com>
2020-12-16 10:55:09 +0000

    test: improved test coverage

--- src/app/core/auth/auth.interceptor.spec.ts
+++ src/app/core/auth/auth.interceptor.spec.ts
@@ -1,18 +1,63 @@
 import { TestBed } from "@angular/core/testing";
 import { MaterialModule } from "../../shared/material/material.module";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+  TestRequest
+} from "@angular/common/http/testing";
+import { CognitoUserSession } from "amazon-cognito-identity-js";
+import { AuthService } from "./auth.service";
 import { AuthInterceptor } from "./auth.interceptor";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { of } from "rxjs";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ConnectionService } from "src/app/connection/services/connection.service";
+import { environment } from "@environment";
 
 describe("AuthInterceptor", () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      providers: [AuthInterceptor],
-      imports: [MaterialModule, HttpClientTestingModule]
-    })
-  );
+  let authService: AuthService;
+  let connectionService: ConnectionService;
+  let httpMock: HttpTestingController;
+  let req: TestRequest;
+  const token = "SDcvxcvxDdsdsLJX343SDSDdssdsds";
 
-  it("should be created", () => {
-    const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
-    expect(interceptor).toBeTruthy();
+  const mockCognitoUserSession: CognitoUserSession = {
+    getIdToken: () => ({
+      payload: { given_name: "Name", family_name: "FName" },
+      getJwtToken: () => token,
+      getIssuedAt: () => null,
+      getExpiration: () => null,
+      decodePayload: () => null
+    }),
+    getAccessToken: () => null,
+    getRefreshToken: () => null,
+    isValid: () => true
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        ConnectionService,
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+      ],
+      imports: [MaterialModule, HttpClientTestingModule]
+    });
+    connectionService = TestBed.inject(ConnectionService);
+    authService = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it("should add jwt token to the request header", () => {
+    const gmcNumber = 65477888;
+    spyOn(authService, "currentSession").and.callFake(() =>
+      of(mockCognitoUserSession)
+    );
+
+    connectionService.getConnectionHistory(gmcNumber).subscribe();
+
+    req = httpMock.expectOne(
+      `${environment.appUrls.getConnections}/${gmcNumber}`
+    );
+    expect(req.request.headers.has("Authorization")).toBe(true);
   });
 });
--- src/app/shared/services/api/api.service.spec.ts
+++ src/app/shared/services/api/api.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from "@angular/core/testing";
+import { ConcernStatus } from "src/app/concern/concern.interfaces";
 
 import { ApiService } from "./api.service";
 
@@ -12,5 +13,9 @@ describe("ApiService", () => {
 
   it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should return the enum value when getEnumKey is invoked", () => {
+    expect(service.getEnumKey(ConcernStatus, ConcernStatus.OPEN)).toBe("OPEN");
   });
 });

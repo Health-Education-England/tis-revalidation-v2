46d219db3f1f8a22ee97d45225a98d692a42c424
Doris Wong <doris.wong@hee.nhs.uk>
2021-01-28 11:38:31 +0000

    feat: pass dbcs in exception summary request url
    TIS21-264

--- src/app/connections/services/connections.service.ts
+++ src/app/connections/services/connections.service.ts
@@ -4,18 +4,25 @@ import { Store } from "@ngxs/store";
 import { RecordsService } from "../../records/services/records.service";
 import { ConnectionsFilterType } from "../connections.interfaces";
 import { ConnectionsStateModel } from "../state/connections.state";
+import { AuthService } from "../../core/auth/auth.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class ConnectionsService {
-  constructor(private store: Store, private recordsService: RecordsService) {}
+  constructor(
+    private store: Store,
+    private recordsService: RecordsService,
+    private authService: AuthService
+  ) {}
 
   public generateParams(): HttpParams {
     const snapshot: ConnectionsStateModel = this.store.snapshot().connections;
     let params: HttpParams = this.recordsService.generateParams(snapshot);
 
-    params = params.append("filter", snapshot.filter);
+    params = params
+      .append("filter", snapshot.filter)
+      .append("dbcs", this.authService.userDesignatedBodies.join(","));
 
     return params;
   }

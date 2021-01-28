f52daa7db86a79edd2e26e7c82ea2a2e9c92abb8
Doris Wong <doris.wong@hee.nhs.uk>
2021-01-28 15:43:14 +0000

    test: add test for dbcs param in exception summary request
    TIS21-264

--- src/app/connections/services/connections.service.spec.ts
+++ src/app/connections/services/connections.service.spec.ts
@@ -7,20 +7,24 @@ import { defaultRecordsState } from "../../records/state/records.state";
 import { ConnectionsFilterType } from "../connections.interfaces";
 import { ConnectionsState } from "../state/connections.state";
 import { ConnectionsService } from "./connections.service";
+import { AuthService } from "../../core/auth/auth.service";
 
 describe("ConnectionsService", () => {
   let connectionsService: ConnectionsService;
   let recordsService: RecordsService;
   let store: Store;
+  let authService: AuthService;
   const sortColumn = "doctorFirstName";
   const sortDirection = "asc";
+  const userLocalOffice = ["1-AIIDWT", "1-AIIDSA", "1-AIIDVS"];
   const connectionsState = {
     ...defaultRecordsState,
     sort: {
       active: sortColumn,
       direction: sortDirection
     },
-    filter: ConnectionsFilterType.DISCONNECTED
+    filter: ConnectionsFilterType.DISCONNECTED,
+    dbcs: userLocalOffice
   };
 
   beforeEach(() => {
@@ -31,9 +35,12 @@ describe("ConnectionsService", () => {
         NgxsModule.forRoot([ConnectionsState])
       ]
     });
+    authService = TestBed.inject(AuthService);
     connectionsService = TestBed.inject(ConnectionsService);
     recordsService = TestBed.inject(RecordsService);
     store = TestBed.inject(Store);
+
+    authService.userDesignatedBodies = userLocalOffice;
 
     store.reset({
       connections: connectionsState
@@ -52,6 +59,7 @@ describe("ConnectionsService", () => {
     expect(recordsService.generateParams).toHaveBeenCalled();
     expect(httpParams.get("sortColumn")).toBe(sortColumn);
     expect(httpParams.get("sortOrder")).toBe(sortDirection);
+    expect(httpParams.get("dbcs")).toBe(userLocalOffice.join(","));
   });
 
   it("`getFilter()` should return `ConnectionsFilterType`", () => {

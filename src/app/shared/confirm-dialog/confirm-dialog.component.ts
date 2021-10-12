8da5f8f689ae124d57d99ad3f145d4ef39dac188
Steven Howard <steven.howard@hee.nhs.uk>
2021-10-12 14:42:37 +0100

    feat:add support for custom button labels to confirm dialog

--- src/app/shared/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -9,13 +9,22 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 export class ConfirmDialogComponent {
   title: string;
   message: string;
+  cancelLabel?: string;
+  confirmLabel?: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: ConfirmDialogModel) {
     this.title = data.title;
     this.message = data.message;
+    this.cancelLabel = data.cancelLabel || "No";
+    this.confirmLabel = data.confirmLabel || "Yes";
   }
 }
 
 export class ConfirmDialogModel {
-  constructor(public title: string, public message: string) {}
+  constructor(
+    public title: string,
+    public message: string,
+    public cancelLabel?: string,
+    public confirmLabel?: string
+  ) {}
 }

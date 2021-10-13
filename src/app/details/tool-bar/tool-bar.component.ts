8699ab941d4bfcdfd7cb369059b06e597999e3d4
Steven Howard <steven.howard@hee.nhs.uk>
2021-10-13 11:35:02 +0100

    feat:add notes count badge to toggle button

--- src/app/details/tool-bar/tool-bar.component.ts
+++ src/app/details/tool-bar/tool-bar.component.ts
@@ -4,7 +4,8 @@ import { Observable } from "rxjs";
 import { map, shareReplay } from "rxjs/operators";
 import { Store, Select } from "@ngxs/store";
 import { ToggleDrawer } from "../notes-drawer/state/notes-drawer.actions";
-
+import { DetailsSideNavState } from "../details-side-nav/state/details-side-nav.state";
+import { IDetailsSideNav } from "../details-side-nav/details-side-nav.interfaces";
 import { NotesDrawerState } from "../notes-drawer/state/notes-drawer.state";
 
 @Component({
@@ -13,6 +14,9 @@ import { NotesDrawerState } from "../notes-drawer/state/notes-drawer.state";
 })
 export class ToolBarComponent implements OnInit {
   @Select(NotesDrawerState.drawerStatus) isOpen$: Observable<boolean>;
+  @Select(DetailsSideNavState.traineeDetails)
+  traineeDetails$: Observable<IDetailsSideNav>;
+  matBadge: number;
   constructor(
     private breakpointObserver: BreakpointObserver,
     private store: Store
@@ -28,5 +32,13 @@ export class ToolBarComponent implements OnInit {
   toggleNotesDrawer() {
     this.store.dispatch(new ToggleDrawer());
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.traineeDetails$.subscribe((traineeDetails) => {
+      if (traineeDetails.notes.length > 0) {
+        this.matBadge = traineeDetails.notes.length;
+      } else {
+        this.matBadge = null;
+      }
+    });
+  }
 }

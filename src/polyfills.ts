7d1aee6a6e1bd76d688944a9523964485e7ceece
Steven Howard <steven.howard@hee.nhs.uk>
2022-06-08 13:44:29 +0100

    Upgrade angular from 11-12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -75,7 +75,7 @@
     });
   }
 })();
-import "zone.js/dist/zone"; // Included with Angular CLI.
+import "zone.js"; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS

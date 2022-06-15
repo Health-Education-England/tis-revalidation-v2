5e6737e23bb6ba4e62c8c687aa07b2ac7043f501
Steven Howard <steven.howard@hee.nhs.uk>
2022-06-15 13:36:59 +0100

    Update relativeLinkResolution to 'corrected'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -55,7 +55,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [
+    RouterModule.forRoot(routes, { relativeLinkResolution: "corrected" })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}

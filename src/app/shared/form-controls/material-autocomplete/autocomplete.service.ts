ae2e5765eaae2ce42e70b91e91ca69b3f099bfc6
Steven Howard <steven.howard@hee.nhs.uk>
2022-09-21 13:32:05 +0100

    Tidy autocomplete service

--- src/app/shared/form-controls/material-autocomplete/autocomplete.service.ts
+++ src/app/shared/form-controls/material-autocomplete/autocomplete.service.ts
@@ -1,9 +1,12 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
 import { AuthService } from "src/app/core/auth/auth.service";
 import { environment } from "@environment";
+
+export interface AutocompleteResults {
+  results: string[];
+}
 
 @Injectable({
   providedIn: "root"
@@ -15,28 +18,9 @@ export class AutocompleteService {
       .set("fieldName", fieldName)
       .set("input", query)
       .set("dbcs", this.authService.userDesignatedBodies?.join(","));
-    return this.http.get<{ results: string[] }>(
+    return this.http.get<AutocompleteResults>(
       `${environment.appUrls.autocomplete}`,
       { params }
-    );
-  }
-
-  private loadNYMovies(query: string): Observable<any[]> {
-    const apiKey: string = "GyIcunqNC1k86GYGU21QAUTdESGlGUOP";
-    const params = new HttpParams().set("api-key", apiKey).set("query", query);
-
-    return this.http.get<any[]>("nymovies", { params }).pipe(
-      map((data: any) => {
-        if (data["results"]) {
-          return data["results"];
-        }
-        return [];
-      }),
-      map((data: any) => {
-        if (data.length) {
-          return data.map((item: any) => item["display_title"]);
-        }
-      })
     );
   }
 }

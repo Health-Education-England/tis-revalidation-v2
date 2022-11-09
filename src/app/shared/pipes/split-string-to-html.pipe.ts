3796e0eea103c8153a32b8cbcf418314aac344dd
Steven Howard <steven.howard@hee.nhs.uk>
2022-11-09 12:17:12 +0000

    fix:no need to bypass domsanitizer

--- src/app/shared/pipes/split-string-to-html.pipe.ts
+++ src/app/shared/pipes/split-string-to-html.pipe.ts
@@ -1,18 +1,16 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
+import { SafeHtml } from "@angular/platform-browser";
 @Pipe({
   name: "splitStringToHTML"
 })
 export class SplitStringToHTMLPipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor() {}
   transform(value: string, delimiter: string = "|"): SafeHtml {
     if (value && value.includes(delimiter)) {
-      return this.sanitizer.bypassSecurityTrustHtml(
-        value
-          .split(delimiter)
-          .map((item: string) => `<span class='split-item'>${item}</span>`)
-          .join(" ")
-      );
+      return value
+        .split(delimiter)
+        .map((item: string) => `<span class='split-item'>${item}</span>`)
+        .join(" ");
     }
     return value;
   }

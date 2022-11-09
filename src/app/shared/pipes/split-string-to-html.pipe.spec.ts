376cc5e10b9c96f86eb67a0690c01753d2efa974
Steven Howard <steven.howard@hee.nhs.uk>
2022-11-09 12:44:08 +0000

    fix: unit test

--- src/app/shared/pipes/split-string-to-html.pipe.spec.ts
+++ src/app/shared/pipes/split-string-to-html.pipe.spec.ts
@@ -1,9 +1,5 @@
 import { SplitStringToHTMLPipe } from "./split-string-to-html.pipe";
-import {
-  BrowserModule,
-  DomSanitizer,
-  SafeHtml
-} from "@angular/platform-browser";
+import { BrowserModule } from "@angular/platform-browser";
 import { TestBed } from "@angular/core/testing";
 describe("SplitStringToHTMLPipe", () => {
   beforeEach(() => {
@@ -13,16 +9,13 @@ describe("SplitStringToHTMLPipe", () => {
   });
 
   it("create an instance", () => {
-    const domSanitizer = TestBed.inject(DomSanitizer);
-    const pipe = new SplitStringToHTMLPipe(domSanitizer);
+    const pipe = new SplitStringToHTMLPipe();
     expect(pipe).toBeTruthy();
   });
 
   it("should split string containing '|' into separate <span> elements ", () => {
-    const domSanitizer = TestBed.inject(DomSanitizer);
-    const pipe = new SplitStringToHTMLPipe(domSanitizer);
-    const safeHtml: SafeHtml = pipe.transform("Hello|World", "|");
-    const text = safeHtml["changingThisBreaksApplicationSecurity"];
+    const pipe = new SplitStringToHTMLPipe();
+    const text = pipe.transform("Hello|World", "|");
     expect(text).toContain("<span class='split-item'>Hello</span>");
     expect(text).toContain("<span class='split-item'>World</span>");
   });

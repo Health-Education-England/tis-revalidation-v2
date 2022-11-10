d1942777732d952f0380f5039a51b7fe31a28931
TISadminCW <TISadminCW@N3647.xhee.nhs.uk>
2022-11-10 09:45:09 +0000

    chore: improved e2e tests for removing interactions
    TIS21-3732: Remove interactions

--- cypress/e2e/app/connections/connection-component/connection-component.cy.ts
+++ cypress/e2e/app/connections/connection-component/connection-component.cy.ts
@@ -3,13 +3,18 @@ describe("Connections Connection Component", () => {
       cy.login();
     });
   
-    const button = "app-update-connetions-btn";
+    const control = "app-update-connetions-btn";
+    const recordListItems = "tr[arialabel=record-list-row]"
   
     it("should allow me to visit the page", () => {
       cy.visit("/connections");
     });
   
     it("should not be able to bulk update connections", () => {
-      cy.get(button).should("not.exist");;
+      cy.visit("/connections");
+      cy.get(recordListItems).eq(0).click();
+      cy.location('pathname', {timeout: 60000})
+        .should('include', '/connection/');
+      cy.get(control).should("not.exist");
     });
   });

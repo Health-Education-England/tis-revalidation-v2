8084b628bf36205d555449844345a5d04d3b8ab1
Steven Howard <steven.howard@hee.nhs.uk>
2022-11-17 13:24:09 +0000

    feat: update table columns

--- src/app/connections/constants.ts
+++ src/app/connections/constants.ts
@@ -1,10 +1,17 @@
-export const COLUMN_DATA: [string, string, boolean][] = [
+const COLUMN_DATA_BASE: [string, string, boolean][] = [
   ["Programme", "programmeName", false],
   ["GMC Submission date", "submissionDate", true],
   ["Designated body", "designatedBody", false],
   ["Programme owner", "programmeOwner", false],
-  ["Programme membership", "programmeMembershipType", false],
-  ["Current connection", "connectionStatus", false],
+  ["Programme membership", "programmeMembershipType", false]
+];
+
+export const CURRENT_COLUMN_DATA: [string, string, boolean][] = [
+  ...COLUMN_DATA_BASE
+];
+
+export const HISTORIC_COLUMN_DATA: [string, string, boolean][] = [
+  ...COLUMN_DATA_BASE,
   ["Start date", "programmeMembershipStartDate", false],
   ["End date", "programmeMembershipEndDate", false]
 ];

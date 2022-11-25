751c9359f37228e97b8d7e64ae1b016cfd33e278
Steven Howard <steven.howard@hee.nhs.uk>
2022-11-25 10:50:52 +0000

    feat: remove unwanted columns from table

--- src/app/connections/constants.ts
+++ src/app/connections/constants.ts
@@ -1,10 +1,7 @@
 export const COLUMN_DATA: [string, string, boolean][] = [
-  ["Programme", "programmeName", false],
+  ["Current programme name", "programmeName", false],
   ["GMC Submission date", "submissionDate", true],
   ["Designated body", "designatedBody", false],
   ["Programme owner", "programmeOwner", false],
-  ["Programme membership", "programmeMembershipType", false],
-  ["Current connection", "connectionStatus", false],
-  ["Start date", "programmeMembershipStartDate", false],
-  ["End date", "programmeMembershipEndDate", false]
+  ["Programme membership", "programmeMembershipType", false]
 ];

02d47947b5df24bdf8b1bf7962ea84ef4c8b1403
Steven Howard <steven.howard@hee.nhs.uk>
2023-03-10 13:04:21 +0000

    chore: update prod environment settings
    NO TICKET

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import { IEnvironment } from "./environment.interface";
+import { APP_URLS_CONFIG, AWS_CONFIG } from "./constants";
 
-export const environment = {
+export const environment: IEnvironment = {
   production: true,
   name: "prod",
   siteIds: ["UA-40570867-6"],
@@ -10,17 +11,15 @@ export const environment = {
   supportLink:
     "https://teams.microsoft.com/l/channel/19%3ac7943c6ffa9c49b881304863bb39ff7b%40thread.skype/General?groupId=102f33a3-f794-4089-8c5a-68e04897e72e&tenantId=ffa7912b-b097-4131-9c0f-d0e80755b2ab",
   dateFormat: "dd/MM/yyyy",
+  appUrls: APP_URLS_CONFIG,
   awsConfig: {
-    domain: "auth.tis.nhs.uk",
-    region: "eu-west-2",
-    scope: ["openid", "aws.cognito.signin.user.admin"],
-    authenticationFlowType: "USER_PASSWORD_AUTH",
-    responseType: "code",
-    userPoolId: "eu-west-2_r3l1XtMDD",
-    userPoolWebClientId: "4o37rm9tbid1u066hr500be5n3",
+    ...AWS_CONFIG,
     bucketName: "tis-revalidation-concerns-upload-prod",
+    domain: "auth.tis.nhs.uk",
     mandatorySignIn: true,
     redirectSignIn: "https://revalidation.tis.nhs.uk",
-    redirectSignOut: "https://revalidation.tis.nhs.uk"
+    redirectSignOut: "https://revalidation.tis.nhs.uk",
+    userPoolId: "eu-west-2_r3l1XtMDD",
+    userPoolWebClientId: "4o37rm9tbid1u066hr500be5n3"
   }
 };

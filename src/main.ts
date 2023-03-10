d839d7a3f8b9c7f17d2af8423bdec08c13930b79
Steven Howard <steven.howard@hee.nhs.uk>
2023-03-10 12:51:51 +0000

    refactor: add preprod env and tidy main.ts
    Remove the dynamic environment variable assignment functionality in
    favour of the native angular environment assignment at build.
    
    Add the preprod environment settings
    
    NO TICKET

--- src/main.ts
+++ src/main.ts
@@ -4,86 +4,55 @@ import { Amplify, Auth } from "aws-amplify";
 import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth/lib-esm/types";
 import { CognitoUserSession } from "amazon-cognito-identity-js";
 import { AppModule } from "./app/app.module";
-import { environment as defaultEnvironment } from "./environments/environment";
+import { environment } from "./environments/environment";
 
-fetch("/api/environment")
-  .then((response) => response.json())
-  .then((envName) => initializeApplication(envName.name))
-  .catch(() => initializeApplication());
+const redirectKey = "reval_redirectLocation";
+const location = window.location;
 
-async function initializeApplication(envName: string = "") {
-  const response = await fetch("./assets/environments/environment.json");
-  const overloadEnvironments = await response.json();
-  const redirectKey = "reval_redirectLocation";
-  const location = window.location;
-
-  if (location.origin.includes("alpha")) {
-    envName = "alpha";
-  }
-  if (
-    !location.origin.includes("localhost") &&
-    envName.length > 0 &&
-    overloadEnvironments &&
-    overloadEnvironments[envName]
-  ) {
-    const overloadEnvironment = overloadEnvironments[envName];
-
-    Object.keys(overloadEnvironment).forEach((key) => {
-      if (
-        defaultEnvironment.hasOwnProperty(key) &&
-        !Array.isArray(defaultEnvironment[key])
-      ) {
-        defaultEnvironment[key] = overloadEnvironment[key];
-      }
-    });
-  }
-
-  if (defaultEnvironment.production) {
-    enableProdMode();
-  }
-
-  const AWS_CONFIG = {
-    Auth: {
-      region: defaultEnvironment.awsConfig.region,
-      userPoolId: defaultEnvironment.awsConfig.userPoolId,
-      userPoolWebClientId: defaultEnvironment.awsConfig.userPoolWebClientId,
-      authenticationFlowType:
-        defaultEnvironment.awsConfig.authenticationFlowType,
-      mandatorySignIn: defaultEnvironment.awsConfig.mandatorySignIn,
-      oauth: {
-        domain: defaultEnvironment.awsConfig.domain,
-        scope: defaultEnvironment.awsConfig.scope,
-        redirectSignIn: defaultEnvironment.awsConfig.redirectSignIn,
-        redirectSignOut: defaultEnvironment.awsConfig.redirectSignOut,
-        responseType: defaultEnvironment.awsConfig.responseType
-      }
-    }
-  };
-
-  Amplify.configure(AWS_CONFIG);
-
-  Auth.currentSession()
-    .then((cognitoUserSession: CognitoUserSession) => {
-      const cognitoIdToken = cognitoUserSession.getIdToken();
-      const roles = cognitoIdToken.payload["cognito:roles"];
-
-      if (roles.includes("HEE Admin Revalidation")) {
-        platformBrowserDynamic()
-          .bootstrapModule(AppModule)
-          .catch((err) => console.error(err));
-      } else {
-        Auth.signOut();
-        window.localStorage.removeItem(redirectKey);
-        alert("Please contact administrator for access");
-      }
-    })
-    .catch(() => {
-      window.localStorage.setItem(
-        redirectKey,
-        `${location.pathname}${location.search}${location.hash}`
-      );
-      Auth.federatedSignIn({
-        provider: CognitoHostedUIIdentityProvider.Cognito
-      });
-    });
+if (environment.production) {
+  enableProdMode();
 }
+
+const AWS_CONFIG = {
+  Auth: {
+    region: environment.awsConfig.region,
+    userPoolId: environment.awsConfig.userPoolId,
+    userPoolWebClientId: environment.awsConfig.userPoolWebClientId,
+    authenticationFlowType: environment.awsConfig.authenticationFlowType,
+    mandatorySignIn: environment.awsConfig.mandatorySignIn,
+    oauth: {
+      domain: environment.awsConfig.domain,
+      scope: environment.awsConfig.scope,
+      redirectSignIn: environment.awsConfig.redirectSignIn,
+      redirectSignOut: environment.awsConfig.redirectSignOut,
+      responseType: environment.awsConfig.responseType
+    }
+  }
+};
+
+Amplify.configure(AWS_CONFIG);
+
+Auth.currentSession()
+  .then((cognitoUserSession: CognitoUserSession) => {
+    const cognitoIdToken = cognitoUserSession.getIdToken();
+    const roles = cognitoIdToken.payload["cognito:roles"];
+
+    if (roles.includes("HEE Admin Revalidation")) {
+      platformBrowserDynamic()
+        .bootstrapModule(AppModule)
+        .catch((err) => console.error(err));
+    } else {
+      Auth.signOut();
+      window.localStorage.removeItem(redirectKey);
+      alert("Please contact administrator for access");
+    }
+  })
+  .catch(() => {
+    window.localStorage.setItem(
+      redirectKey,
+      `${location.pathname}${location.search}${location.hash}`
+    );
+    Auth.federatedSignIn({
+      provider: CognitoHostedUIIdentityProvider.Cognito
+    });
+  });

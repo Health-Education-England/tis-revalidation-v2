b108849c6d3b0bcb4bf26e25e031f8d831554584
Steven Howard <steven.howard@hee.nhs.uk>
2023-04-05 11:45:51 +0100

    refactor: update envs to remove duplication
    The duplications in environment variables fails the SonarCloud
    Quality Gate. Updated to use shared settings between
    multiple environments.
    
    NO TICKET

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,38 +1,7 @@
-import { APP_URLS_CONFIG, AWS_CONFIG } from "./constants";
+import { commonEnv } from "./environment.common";
 import { IEnvironment } from "./environment.interface";
 
-export const environment: IEnvironment = {
-  production: false,
-  name: "dev",
-  siteIds: ["UA-40570867-6"],
-  hotJarId: 1662399,
-  hotJarSv: 6,
-  adminsUIHostUri: `https://stage-apps.tis.nhs.uk/`,
-  supportLink: `https://teams.microsoft.com/l/channel/19%3ac7943c6ffa9c49b881304863bb39ff7b%40thread.skype/General?groupId=102f33a3-f794-4089-8c5a-68e04897e72e&tenantId=ffa7912b-b097-4131-9c0f-d0e80755b2ab`,
-  dateFormat: "dd/MM/yyyy",
-  appUrls: APP_URLS_CONFIG,
-  londonDBCs: [
-    {
-      key: "1-AIIDR8",
-      value: "Kent, Surrey and Sussex"
-    },
-    {
-      key: "1-AIIDVS",
-      value: "North Central and East London"
-    },
-    { key: "1-AIIDWA", value: "North West London" },
-    { key: "1-AIIDWI", value: "South London" }
-  ],
-  awsConfig: {
-    ...AWS_CONFIG,
-    bucketName: "tis-revalidation-concerns-upload-preprod",
-    domain: "stage-auth.tis.nhs.uk",
-    redirectSignIn: "http://localhost:4200",
-    redirectSignOut: "http://localhost:4200",
-    userPoolId: "eu-west-2_o5Es6Bpl8",
-    userPoolWebClientId: "r28c0u6jpc7scm9e2anhh274r"
-  }
-};
+export const environment: IEnvironment = commonEnv;
 
 /*
  * For easier debugging in development mode, you can import the following file

4a5ec0621aa31eae2958c3a786827607d4914d3f
Steven Howard <steven.howard@hee.nhs.uk>
2023-04-24 16:20:41 +0100

    refactor:assert that connection page is loaded.
    I believe tests were failing due to the login not completing
    prior to the test running so that the recommendation page
    opened after the visit connections page action.  Hoping the
    assertion that waits for the table to display after login
    and prior to the tests running will fix.
    
    Also added init connection page function and asserion
    that it is the connection page.
    
    NO TICKET

--- cypress/e2e/app/connections/connection-component/connection-component.cy.ts
+++ cypress/e2e/app/connections/connection-component/connection-component.cy.ts
@@ -1,43 +1,54 @@
 describe("Connections Connection Component", () => {
-    beforeEach(() => {
-      cy.loginSession();
-    });
-
-    const updateComponent = "app-update-connection";
-    const bulkUpdateControl = "app-update-connetions-btn";
-    const connectionHistoryTable = "[data-cy='connection-history-table']"
-    const programmeHistoryTable = "[data-cy='programme-history-table']"
-    const recordListItems = "tr[arialabel=record-list-row]"
-
-    it("should not be able to bulk update connections", () => {
-      cy.visit("/connections");
-      cy.get(recordListItems).eq(0).click();
-      cy.location('pathname', {timeout: 60000})
-        .should('include', '/connection/');
-      cy.get(bulkUpdateControl).should("not.exist");
-    });
-
-    it("should not be able to update connections", () => {
-      cy.visit("/connections");
-      cy.get(recordListItems).eq(0).click();
-      cy.location('pathname', {timeout: 60000})
-        .should('include', '/connection/');
-      cy.get(updateComponent).should("not.exist");
-    });
-
-    it("should not be able to see connection history", () => {
-      cy.visit("/connections");
-      cy.get(recordListItems).eq(0).click();
-      cy.location('pathname', {timeout: 60000})
-        .should('include', '/connection/');
-      cy.get(connectionHistoryTable).should("exist");
-    });
-
-    it("should not be able to see programme history", () => {
-      cy.visit("/connections");
-      cy.get(recordListItems).eq(0).click();
-      cy.location('pathname', {timeout: 60000})
-        .should('include', '/connection/');
-      cy.get(programmeHistoryTable).should("not.exist");
-    });
+  beforeEach(() => {
+    cy.loginSession();
   });
+
+  const updateComponent = "app-update-connection";
+  const bulkUpdateControl = "app-update-connetions-btn";
+  const connectionHistoryTable = "[data-cy='connection-history-table']";
+  const programmeHistoryTable = "[data-cy='programme-history-table']";
+  const recordListItems = "tr[arialabel=record-list-row]";
+  const initConnectionsPage = () => {
+    cy.visit("/connections");
+    cy.get(".mat-tab-link").first().should("contain", "CONNECTIONS");
+  };
+  it("should not be able to bulk update connections", () => {
+    initConnectionsPage();
+    cy.get(recordListItems).eq(0).click();
+    cy.location("pathname", { timeout: 60000 }).should(
+      "include",
+      "/connection/"
+    );
+    cy.get(bulkUpdateControl).should("not.exist");
+  });
+
+  it("should not be able to update connections", () => {
+    initConnectionsPage();
+    cy.get(recordListItems).eq(0).click();
+    cy.location("pathname", { timeout: 60000 }).should(
+      "include",
+      "/connection/"
+    );
+    cy.get(updateComponent).should("not.exist");
+  });
+
+  it("should not be able to see connection history", () => {
+    initConnectionsPage();
+    cy.get(recordListItems).eq(0).click();
+    cy.location("pathname", { timeout: 60000 }).should(
+      "include",
+      "/connection/"
+    );
+    cy.get(connectionHistoryTable).should("exist");
+  });
+
+  it("should not be able to see programme history", () => {
+    initConnectionsPage();
+    cy.get(recordListItems).eq(0).click();
+    cy.location("pathname", { timeout: 60000 }).should(
+      "include",
+      "/connection/"
+    );
+    cy.get(programmeHistoryTable).should("not.exist");
+  });
+});

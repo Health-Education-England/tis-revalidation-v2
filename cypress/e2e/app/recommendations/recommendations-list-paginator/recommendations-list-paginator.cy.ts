3d0e6df98cf6a8b3f0aaddf0fac5184280bc4a9e
Steven Howard <steven.howard@hee.nhs.uk>
2023-05-17 20:16:02 +0100

    test: tidy all E2E tests
    Remove redundant duplicate tests and fix existing tests
    following changes to filter button data-cy attribute. Also,
    tidy folder structure
    
    TIS21-3734: Ability to disconnect a doctor

--- cypress/e2e/app/recommendations/recommendations-list-paginator/recommendations-list-paginator.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("Recommendations list paginator", () => {
-  before(() => {
-    cy.login();
-  });
-
-  it("should allow me to visit the page", () => {
-    cy.visit("/recommendations");
-  });
-});
--- cypress/e2e/app/recommendations/reset-recommendations-list/reset-recommendations-list.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("Reset recommendations list", () => {
-  before(() => {
-    cy.login();
-  });
-
-  it("should allow me to visit the page", () => {
-    cy.visit("/recommendations");
-  });
-});

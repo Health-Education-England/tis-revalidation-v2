16de288e7ce86baaf4b109aeca2bd70a5b72a358
Steven Howard <steven.howard@hee.nhs.uk>
2023-06-12 13:53:39 +0100

    wip: add column sorting

--- src/app/connections/constants.ts
+++ src/app/connections/constants.ts
@@ -4,12 +4,12 @@ import {
   FormControlType
 } from "../shared/form-controls/form-contol-base.model";
 export const COLUMN_DATA: [string, string, boolean][] = [
-  ["Current programme name", "programmeName", false],
-  ["GMC Submission date", "submissionDate", false],
-  ["GMC Designated body", "designatedBody", false],
-  ["TIS Designated body", "tcsDesignatedBody", false],
-  ["Programme owner", "programmeOwner", false],
-  ["Programme membership", "programmeMembershipType", false]
+  ["Current programme name", "programmeName", true],
+  ["GMC Submission date", "submissionDate", true],
+  ["GMC Designated body", "designatedBody", true],
+  ["TIS Designated body", "tcsDesignatedBody", true],
+  ["Programme owner", "programmeOwner", true],
+  ["Programme membership", "programmeMembershipType", true]
 ];
 
 export const TABLE_FILTERS_FORM_BASE: Array<

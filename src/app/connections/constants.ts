afe9385c1f6a5aeefa53d656e3b4a2253e454675
Steven Howard <steven.howard@hee.nhs.uk>
2023-06-16 11:42:56 +0100

    feat: remove TIS Designated body column
    This column is derived from the programme owner so
    displays the same data in different formats. Since the
    programme owner is now displayed in same format as
    GMC Designated body, the 2 can be easily compared and
    TIS Designated body column no longer required.
    
    Lisa also mentioned in the user research session that the
    label is misleading since TIS does not have designated
    bodies.
    
    TIS21-4655: Display the DBC as an abbreviation instead
    of code in summary records table

--- src/app/connections/constants.ts
+++ src/app/connections/constants.ts
@@ -7,7 +7,6 @@ export const COLUMN_DATA: [string, string, boolean][] = [
   ["Current programme name", "programmeName", false],
   ["GMC Submission date", "submissionDate", false],
   ["GMC Designated body", "designatedBody", false],
-  ["TIS Designated body", "tcsDesignatedBody", false],
   ["Programme owner", "programmeOwner", false],
   ["Programme membership", "programmeMembershipType", false]
 ];

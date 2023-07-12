5cc64922a4198fbd7f6159ff7e4b16809d0d84df
Steven Howard <steven.howard@hee.nhs.uk>
2023-07-12 16:03:27 +0100

    test:fix unit test

--- src/app/core/http-error/http-error.interceptor.spec.ts
+++ src/app/core/http-error/http-error.interceptor.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from "@angular/core/testing";
 import { MaterialModule } from "../../shared/material/material.module";
 import { HttpErrorInterceptor } from "./http-error.interceptor";
-
+import { RouterTestingModule } from "@angular/router/testing";
 describe("HttpErrorInterceptor", () => {
   let interceptor: HttpErrorInterceptor;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [MaterialModule],
+      imports: [MaterialModule, RouterTestingModule],
       providers: [HttpErrorInterceptor]
     });
     interceptor = TestBed.inject(HttpErrorInterceptor);

d89bf369836716aa7a0785b7da1755b270acab52
Steven Howard <steven.howard@hee.nhs.uk>
2023-07-21 14:46:01 +0100

    fix: remove programe from connection response
    Programme history was previously removed from connection
    detail page in the UI. It has now been removed from the response
    as redundant so this cleans up the UI to remove references.
    
    TIS21-4548: Correctly handle situations where a doctor exists in GMC
    but not in TIS on the connection and recommendation details pages

--- src/app/connection/state/connection.actions.ts
+++ src/app/connection/state/connection.actions.ts
@@ -1,6 +1,3 @@
-import { HttpErrorPayload } from "../../shared/services/error/error.service";
-import { IProgramme } from "../connection.interfaces";
-
 export class Get {
   static readonly type = "[Connection] Get";
   constructor(public payload: number) {}

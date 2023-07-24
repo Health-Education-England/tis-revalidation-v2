2bc831f7f409dc00e5c022ac827040208c62bb0f
Steven Howard <steven.howard@hee.nhs.uk>
2023-07-24 13:17:48 +0100

    chore: ng update @angular/material@15
    NO TICKET

--- src/app/details/comments/comments.component.ts
+++ src/app/details/comments/comments.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { UntypedFormArray, UntypedFormGroup } from "@angular/forms";
 import { CommentsService } from "./comments.service";
-import { MatCheckboxChange } from "@angular/material/checkbox";
-import { MatDialogRef, MatDialog } from "@angular/material/dialog";
+import { MatLegacyCheckboxChange as MatCheckboxChange } from "@angular/material/legacy-checkbox";
+import { MatLegacyDialogRef as MatDialogRef, MatLegacyDialog as MatDialog } from "@angular/material/legacy-dialog";
 
 @Component({
   selector: "app-comments",
--- src/app/records/record-list-paginator/record-list-paginator.component.ts
+++ src/app/records/record-list-paginator/record-list-paginator.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { PageEvent } from "@angular/material/paginator";
+import { LegacyPageEvent as PageEvent } from "@angular/material/legacy-paginator";
 import { Store } from "@ngxs/store";
 import { Observable } from "rxjs";
 import { take } from "rxjs/operators";

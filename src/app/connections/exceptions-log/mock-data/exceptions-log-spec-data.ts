dffa56b06c4fd2ceca22a59a793fa7202e4d0763
StevenHee <steven.howard@hee.nhs.uk>
2023-10-20 13:13:42 +0100

    test: fix unit test
    Table data sorted by timestamp descending
    
    TIS21-5151: Show log of errors when adding/removing connections

--- src/app/connections/exceptions-log/mock-data/exceptions-log-spec-data.ts
+++ src/app/connections/exceptions-log/mock-data/exceptions-log-spec-data.ts
@@ -2,12 +2,7 @@ import { IException } from "../exceptions-log.interface";
 
 export const exceptionsJson = `
 [
-    {
-        "gmcId": "1",
-        "errorMessage": "Error message",
-        "timestamp": "2023-10-18T14:45:43.793",
-        "admin": "admin"
-    },
+   
     {
         "gmcId": "1",
         "errorMessage": "Error message",
@@ -18,6 +13,12 @@ export const exceptionsJson = `
         "gmcId": "3",
         "errorMessage": "Error message",
         "timestamp": "2023-10-18T14:45:47.337",
+        "admin": "admin"
+    },
+    {
+        "gmcId": "1",
+        "errorMessage": "Error message",
+        "timestamp": "2023-10-18T14:45:43.793",
         "admin": "admin"
     }
 

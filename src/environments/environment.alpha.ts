efff5654f2bcb18a7f289b6ee02aadbe6a8ec2e1
StevenHee <steven.howard@hee.nhs.uk>
2024-10-10 09:45:01 +0100

    feat: update cognito config to use TIS user pools
    TIS21-2486: Migrate revalidation to TIS Cognito user pools

--- src/environments/environment.alpha.ts
+++ src/environments/environment.alpha.ts
@@ -6,6 +6,9 @@ const env: Partial<IEnvironment> = {
   production: true,
   gtmID: "GTM-WFRHL895",
   awsConfig: {
+    domain: "tis-preprod.auth.eu-west-2.amazoncognito.com",
+    userPoolId: "eu-west-2_sNYQvAsaG",
+    userPoolWebClientId: "12apcktrr55idvu3hdhga9p1e5",
     redirectSignIn: "https://alpha-revalidation.tis.nhs.uk",
     redirectSignOut: "https://alpha-revalidation.tis.nhs.uk"
   }

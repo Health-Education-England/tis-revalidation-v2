2c900c68ac6843d669c5358f499d6eebacb7801f
StevenHEE <steven.howard@hee.nhs.uk>
2026-01-08 09:53:47 +0000

    test: fix unit tests
    TIS21-7921
    TIS21-8134

--- src/app/shared/form-controls/material-autocomplete/autocomplete.service.spec.ts
+++ src/app/shared/form-controls/material-autocomplete/autocomplete.service.spec.ts
@@ -4,28 +4,69 @@ import {
   HttpTestingController
 } from "@angular/common/http/testing";
 import { AutocompleteService } from "./autocomplete.service";
+import { NgxsModule, Store } from "@ngxs/store";
+import { AdminsState } from "src/app/admins/state/admins.state";
+import { MaterialModule } from "../../material/material.module";
 
 describe("AutocompleteService", () => {
   let service: AutocompleteService;
+  let store: Store;
   let httpTestingController: HttpTestingController;
   const options: string[] = ["apple", "banana", "cherry"];
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [AutocompleteService],
-      imports: [HttpClientTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        NgxsModule.forRoot([AdminsState]),
+        MaterialModule
+      ]
     });
-
+    store = TestBed.inject(Store);
     service = TestBed.inject(AutocompleteService);
     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it("should call correct endpoint", () => {
+  it("should call correct endpoint when calling getItems()", () => {
     service.getItems("programmeName", "query").subscribe();
     const request = httpTestingController.expectOne(
       "api/v1/doctors/autocomplete?fieldName=programmeName&input=query&dbcs="
     );
-
     request.flush(options);
     expect(request.request.method).toBe("GET");
+  });
+
+  it("should return expected data when calling filterItems()", () => {
+    const items = ["Dixon Bainbridge", "Bob Fossil", "Howard Moon"];
+    service.filterItems("how", items).subscribe((matches) => {
+      expect(matches.includes("Howard Moon")).toBeTrue();
+    });
+  });
+
+  it("should return expected data when calling getAdmins()", () => {
+    store.reset({
+      admins: {
+        items: [
+          {
+            fullName: "Bob Fossil",
+            email: "bob.fossil@hee.nhs.uk",
+            username: "bob.fossil@hee.nhs.uk"
+          },
+          {
+            fullName: "Dixon Bainbridge",
+            email: "dixon.bainbridge@hee.nhs.uk",
+            username: "dixon.bainbridge@hee.nhs.uk"
+          },
+          {
+            fullName: "Howard Moon",
+            email: "howard.moon@hee.nhs.uk",
+            username: "howard.moon@@hee.nhs.uk"
+          }
+        ]
+      }
+    });
+    service.getAdmins({ query: "how" }).subscribe((matches) => {
+      expect(matches.includes("Howard Moon")).toBeTrue();
+    });
   });
 });

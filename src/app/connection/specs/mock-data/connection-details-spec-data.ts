aa8442be885cca9823237cf44f5607b4e8fbf415
StevenHEE <steven.howard@hee.nhs.uk>
2026-05-07 15:43:24 +0100

    chore: false positive duplication flagged in sonarqube
    TIS21-8544

--- src/app/connection/specs/mock-data/connection-details-spec-data.ts
+++ src/app/connection/specs/mock-data/connection-details-spec-data.ts
@@ -45,46 +45,6 @@ const hiddenDiscrepancies: IHiddenDiscrepancy[] = [
     reason:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget mi ultrices, aliquam lectus et, faucibus magna.",
     hiddenDateTime: "2026-03-31T09:52:04.703"
-  },
-  {
-    id: "69e73b703bd79b659f0c1528",
-    gmcId: "234567",
-    hiddenForDesignatedBodyCode: "1-1RSSQ05",
-    hiddenBy: "Bob Fossil",
-    reason: "dfgdfg",
-    hiddenDateTime: "2026-04-21T08:55:12.879"
-  },
-  {
-    id: "69e73ddd3bd79b659f0c152f",
-    gmcId: "345678",
-    hiddenForDesignatedBodyCode: "1-1RSSPZ7",
-    hiddenBy: "Dixon Bainbridge",
-    reason: "Test",
-    hiddenDateTime: "2026-04-21T09:05:33.948"
-  },
-  {
-    id: "69e771703bd79b659f0c1541",
-    gmcId: "4567890",
-    hiddenForDesignatedBodyCode: "1-1RUZUYF",
-    hiddenBy: "test",
-    reason: "test",
-    hiddenDateTime: "2026-04-21T12:45:36.893"
-  },
-  {
-    id: "69e7784f3bd79b659f0c1542",
-    gmcId: "5678901",
-    hiddenForDesignatedBodyCode: "1-1RSSQ5L",
-    hiddenBy: "test",
-    reason: "test",
-    hiddenDateTime: "2026-04-21T13:14:55.409"
-  },
-  {
-    id: "69e781a13bd79b659f0c1572",
-    gmcId: "6789012",
-    hiddenForDesignatedBodyCode: "1-1RUZV1D",
-    hiddenBy: "test",
-    reason: "test",
-    hiddenDateTime: "2026-04-21T13:54:41.82"
   }
 ];
 
